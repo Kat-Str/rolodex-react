fd8c27a5c0f8f52bd94664a2d683b0b19e4d8a31
Kat-Str <120758963+Kat-Str@users.noreply.github.com>
2023-07-28 10:07:41 +0200

    Added user filtering based on search input

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import { Component } from "react";
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
       users: [],
+      searchField: "",
     };
   }
 
@@ -23,9 +25,24 @@ class App extends Component {
   }
 
   render() {
+    const filteredUsers = this.state.users.filter((user) => {
+      return user.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.users.map((user) => {
+        <input
+          type="search"
+          placeholder="Search users"
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+
+        {filteredUsers.map((user) => {
           return (
             <div key={user.id}>
               <h2>{user.name}</h2>
